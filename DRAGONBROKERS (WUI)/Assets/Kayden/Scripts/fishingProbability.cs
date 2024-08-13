723644b25f10380fc44276faf5a93b435c8588e9
kdennnn <kdenleong@gmail.com>
2024-08-13 14:27:54 +0800

    Fixed probability script and made slight changes to new scene

--- DRAGONBROKERS (WUI)/Assets/Kayden/Scripts/fishingProbability.cs
+++ DRAGONBROKERS (WUI)/Assets/Kayden/Scripts/fishingProbability.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class fishingProbability : MonoBehaviour
 {
-    public float SeasonalChance = 0.7f;
+    public float SeasonalChance = 0.6f;
     public float RareChance = 0.29f;
+    public float NothingChance = 0.1f;
     public float LegendaryChance = 0.01f;
 
     // Method to determine which animation to play
@@ -14,18 +15,29 @@ public class fishingProbability : MonoBehaviour
         // Generate a random float between 0.0 and 1.0
         float chance = Random.value;
 
+        // Calculate cumulative probabilities
+        float seasonalThreshold = SeasonalChance;
+        float rareThreshold = seasonalThreshold + RareChance;
+        float nothingThreshold = rareThreshold + NothingChance;
+        float legendaryThreshold = nothingThreshold + LegendaryChance;
+
         // Determine the animation to play based on the chance and thresholds
-        if (chance < SeasonalChance)
+        if (chance < seasonalThreshold)
         {
             playerAnim.Play("playerWonFish");
             Debug.Log("Animation Played: 1");
         }
-        else if (chance < RareChance)
+        else if (chance < rareThreshold)
         {
             playerAnim.Play("playerWonFish2");
             Debug.Log("Animation Played: 2");
         }
-        else if (chance < LegendaryChance)
+        else if (chance < nothingThreshold)
+        {
+            playerAnim.Play("playerStill");
+            Debug.Log("Animation Played: No fish caught");
+        }
+        else if (chance < legendaryThreshold)
         {
             playerAnim.Play("playerWonFish3");
             Debug.Log("Animation Played: 3");

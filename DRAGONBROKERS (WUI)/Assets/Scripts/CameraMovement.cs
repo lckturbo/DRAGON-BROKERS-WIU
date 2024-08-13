805d74e6b71c63d27218f4269c9ebaa77c302fe9
lckturbo <chengkel341@gmail.com>
2024-08-13 10:03:10 +0800

    updates to player and camera

---
+++ DRAGONBROKERS (WUI)/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                                                target.position, smoothing);
+        }
+
+
+    }
+}
---
+++ DRAGONBROKERS (WUI)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+
+        // Check for horizontal input first
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            change.x = Input.GetAxisRaw("Horizontal");
+        }
+        // If no horizontal input, then check for vertical input
+        else if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            change.y = Input.GetAxisRaw("Vertical");
+        }
+
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        if (change != Vector3.zero)
+        {
+            myRigidbody.MovePosition(
+                transform.position + change * speed * Time.fixedDeltaTime
+            );
+        }
+    }
+}

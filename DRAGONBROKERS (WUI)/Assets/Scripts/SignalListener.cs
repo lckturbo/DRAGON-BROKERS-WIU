92c8fe2291b06b4678b71f10f51600d760a5453d
lckturbo <chengkel341@gmail.com>
2024-08-16 10:57:00 +0800

    yay

---
+++ DRAGONBROKERS (WUI)/Assets/Scripts/SignalListener.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalListener : MonoBehaviour
+{
+    public Signal signal;
+    public UnityEvent signalEvent;
+
+    public void OnSignalRaised()
+    {
+        signalEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+       signal.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        signal.DeRegisterListener(this); 
+    }
+}

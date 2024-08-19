7ae27f8907ef67e9075bb49fe358bc512618414e
kdennnn <kdenleong@gmail.com>
2024-08-19 15:45:52 +0800

    Polished trawling scene

---
+++ DRAGONBROKERS (WUI)/Assets/Kayden/Scripts/Trawling/TrawlTimelineController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TrawlTimelineController : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public IKManager ikManager;
+
+    void Start()
+    {
+        if (timeline != null)
+        {
+            timeline.stopped += OnTimelineStopped;
+        }
+    }
+
+    void Update()
+    {
+        if (timeline != null && timeline.state == PlayState.Playing)
+        {
+            DisablePlayerControls();
+        }
+    }
+
+    void DisablePlayerControls()
+    {
+        if (ikManager != null)
+        {
+            ikManager.enabled = false;
+        }
+    }
+
+    void OnTimelineStopped(PlayableDirector director)
+    {
+        EnablePlayerControls();
+    }
+
+    void EnablePlayerControls()
+    {
+        if (ikManager != null)
+        {
+            ikManager.enabled = true;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (timeline != null)
+        {
+            timeline.stopped -= OnTimelineStopped;
+        }
+    }
+}

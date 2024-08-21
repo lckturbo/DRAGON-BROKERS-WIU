039639ba6fdae58f0a5448fe3242446837c33797
JJ <dingjj08@gmail.com>
2024-08-21 08:18:35 +0800

    Added day changing, added fish food check

--- DRAGONBROKERS (WUI)/Assets/Axl/Scripts/SceneSwitching.cs
+++ DRAGONBROKERS (WUI)/Assets/Axl/Scripts/SceneSwitching.cs
@@ -4,13 +4,17 @@ using UnityEngine.SceneManagement;
 public class SceneSwitching : MonoBehaviour
 {
     public int sceneIndex;
-
     private EnergyDepletion energyDepletion;
     public InventoryManager inventoryManager;
 
     private void Start()
     {
         energyDepletion = GetComponent<EnergyDepletion>();
+
+        if (energyDepletion == null)
+        {
+            Debug.LogError("EnergyDepletion component not found on this GameObject!");
+        }
     }
 
     private void Update()
@@ -21,9 +25,10 @@ public class SceneSwitching : MonoBehaviour
             SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
         }
 
-        if (energyDepletion.stopTimer == true)
+        if (energyDepletion != null && energyDepletion.stopTimer == true)
         {
             SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+            Debug.Log("Scene switched to index: " + sceneIndex);
         }
     }
 }

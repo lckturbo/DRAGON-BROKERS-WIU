1f4cc7928d45beb9fdd11c4cdc744b43e7d87f43
JJ <dingjj08@gmail.com>
2024-08-21 17:45:36 +0800

    Added booleans to buy and sell
    Removed debug script
    Added the buttons to the function

--- DRAGONBROKERS (WUI)/Assets/JJ/Scripts/Shop_UI_Scripts/BuyItems.cs
+++ DRAGONBROKERS (WUI)/Assets/JJ/Scripts/Shop_UI_Scripts/BuyItems.cs
@@ -6,13 +6,61 @@ public class BuyItems : MonoBehaviour
 {
     private GoldManager goldManager;
     private FishFoodManager fishFoodManager;
+    private WeightManager weightManager;
 
-    public bool poisonActive = false;
+    public bool poisonActive = false; //Poison
+    public bool trawlActive = false; //Trawling Boat Add on
+    public bool IncreaseWeight = false; //Max Weight Increase Add on
+
+    public bool PlatRod = false;
+    public bool EmeRod = false;
+    public bool IRod = false;
 
     private void Start()
     {
         goldManager = GameObject.FindObjectOfType<GoldManager>();
+        if (goldManager != null) 
+        {
+            Debug.Log("GoldManager not found");
+        }
+
         fishFoodManager = GameObject.FindObjectOfType<FishFoodManager>();
+        if(fishFoodManager != null)
+        {
+            Debug.Log("FishFoodManager not found");
+        }
+
+        weightManager = GameObject.FindObjectOfType<WeightManager>();
+        if (weightManager != null)
+        {
+            Debug.Log("WeightManager not found");
+        }
+    }
+
+    public void OnBuyTrawling()
+    {
+        if (goldManager.goldCount >= 300)
+        {
+            goldManager.goldCount -= 300;
+            trawlActive = true;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for Trawling");
+        }
+    }
+
+    public void OnBuyMoreWeight()
+    {
+        if (goldManager.goldCount >= 800)
+        {
+            goldManager.goldCount -= 800;
+            IncreaseWeight = true;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for Space Upgrade Within Inventory");
+        }
     }
 
     public void OnBuyPoison()
@@ -30,7 +78,53 @@ public class BuyItems : MonoBehaviour
 
     public void BuyFishFood()
     {
-        goldManager.goldCount -= 10;
-        fishFoodManager.foodCount += 3;
+        if (goldManager.goldCount >= 10)
+        {
+            goldManager.goldCount -= 10;
+            fishFoodManager.foodCount += 3;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for Fish Food");
+        }
+    }
+
+    public void BuyPlatRod()
+    {
+        if (goldManager.goldCount >= 100)
+        {
+            goldManager.goldCount -= 100;
+            PlatRod = true;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for Plat Rod");
+        }
+    }
+
+    public void BuyEmeRod()
+    {
+        if (goldManager.goldCount >= 400)
+        {
+            goldManager.goldCount -= 400;
+            EmeRod = true;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for EmeRod");
+        }
+    }
+
+    public void BuyIRod()
+    {
+        if (goldManager.goldCount >= 600)
+        {
+            goldManager.goldCount -= 600;
+            IRod = true;
+        }
+        else
+        {
+            Debug.Log("Not Enough Money for IRod");
+        }
     }
 }
